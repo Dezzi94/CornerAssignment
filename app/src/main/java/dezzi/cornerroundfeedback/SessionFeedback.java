b95f153dce2c453bb780e53360a5a3df33744bf0
Jack <techidesmond@gmail.com>
2018-03-31 14:33:33 +0100

    Initial commit. Starting template with background colours changed.

---
+++ app/src/main/java/dezzi/cornerroundfeedback/SessionFeedback.java
@@ -0,0 +1,13 @@
+package dezzi.cornerroundfeedback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_feedback);
+    }
+}
