ad24dd9bd376459bf304e8e92295b740bb0b8f15
Jack <techidesmond@gmail.com>
2018-04-02 21:17:58 +0100

    Created Navigation Drawer And Added av Header File As Well AS String.xml File For Accessibility.

--- app/src/main/java/dezzi/cornerroundfeedback/SessionFeedback.java
+++ app/src/main/java/dezzi/cornerroundfeedback/SessionFeedback.java
@@ -1,7 +1,9 @@
 package dezzi.cornerroundfeedback;
 
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class SessionFeedback extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SessionFeedback extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_feedback);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle
     }
 }
